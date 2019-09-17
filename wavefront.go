0310739dc7f2ca9cf57c2d52f32c47ab910b1db4
roz3x <oroz3x@gmail.com>
2019-09-17 16:48:25 -0400

    changed format

--- wavefront.go
+++ wavefront.go
@@ -10,63 +10,63 @@ import (
 
 //Wavefront the file handler
 type Wavefront struct {
-	lines             [][]byte
-	coordinatesPoints [][]float64
-	lineindexs        [][]int
-	vecternormal      [][]float64
-	faces             [][][]int
+	Lines            [][]byte
+	CoordinatePoints [][]float64
+	LineIndices      [][]int
+	VertexNormals    [][]float64
+	Faces            [][][]int
 }
 
 //Init function that does all the work
 func (f *Wavefront) Init(file io.Reader) {
 
 	var (
-		linesregex       = regexp.MustCompile(`.*\n`)
-		pointlinetest    = regexp.MustCompile(`^v\ `)
-		pointtest        = regexp.MustCompile(`[\d]*\.[\d]*`)
-		linelinetest     = regexp.MustCompile(`^l`)
-		linetest         = regexp.MustCompile(`[\d]+`)
-		vecterNormalLine = regexp.MustCompile(`^vn`)
-		facelinetest     = regexp.MustCompile(`^f`)
-		faceblocktest    = regexp.MustCompile(`[\d|/]+`)
-		facesettest      = regexp.MustCompile(`[\d]+`)
+		Linesregex        = regexp.MustCompile(`.*\n`)
+		pointlinetest     = regexp.MustCompile(`^v\ `)
+		pointtest         = regexp.MustCompile(`[\d]*\.[\d]*`)
+		linelinetest      = regexp.MustCompile(`^l`)
+		linetest          = regexp.MustCompile(`[\d]+`)
+		VertexNormalsLine = regexp.MustCompile(`^vn`)
+		facelinetest      = regexp.MustCompile(`^f`)
+		faceblocktest     = regexp.MustCompile(`[\d|/]+`)
+		Facesettest       = regexp.MustCompile(`[\d]+`)
 	)
 	filebody, err := ioutil.ReadAll(file)
 	handle(err)
 	// this line will remove error related to last missing newline character
 	filebody = append(filebody, []byte("\n")...)
-	f.lines = linesregex.FindAll(filebody, -1)
-	for _, curLine := range f.lines {
+	f.Lines = Linesregex.FindAll(filebody, -1)
+	for _, curLine := range f.Lines {
 		if pointlinetest.Match(curLine) {
 			tmp := []float64{}
 			for _, k := range pointtest.FindAll(curLine, -1) {
 				tmp = append(tmp, convFloat(strconv.ParseFloat(string(k), 64)))
 			}
-			f.coordinatesPoints = append(f.coordinatesPoints, tmp)
+			f.CoordinatePoints = append(f.CoordinatePoints, tmp)
 		} else if linelinetest.Match(curLine) {
 			tmp := []int{}
 			for _, k := range linetest.FindAll(curLine, -1) {
 				tmp = append(tmp, convInt(strconv.ParseInt(string(k), 10, 64)))
 			}
-			f.lineindexs = append(f.lineindexs, tmp)
-		} else if vecterNormalLine.Match(curLine) {
+			f.LineIndices = append(f.LineIndices, tmp)
+		} else if VertexNormalsLine.Match(curLine) {
 			tmp := []float64{}
 			for _, k := range pointtest.FindAll(curLine, -1) {
 				tmp = append(tmp, convFloat(strconv.ParseFloat(string(k), 64)))
 			}
-			f.vecternormal = append(f.vecternormal, tmp)
+			f.VertexNormals = append(f.VertexNormals, tmp)
 		} else if facelinetest.Match(curLine) {
 			tmp := faceblocktest.FindAll(curLine, -1)
 			tmpfarr := [][]int{}
 			for _, i := range tmp {
-				p := facesettest.FindAll(i, -1)
+				p := Facesettest.FindAll(i, -1)
 				tmparr := []int{}
 				for _, k := range p {
 					tmparr = append(tmparr, convInt(strconv.ParseInt(string(k), 10, 64)))
 				}
 				tmpfarr = append(tmpfarr, tmparr)
 			}
-			f.faces = append(f.faces, tmpfarr)
+			f.Faces = append(f.Faces, tmpfarr)
 		}
 	}
 }
